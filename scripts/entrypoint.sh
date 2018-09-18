ee081fbd0bd6938d7fc7fa4d7446e0f243af06f7
Ramon Tayag <ramon.tayag@gmail.com>
2018-09-18 13:59:15 +0800

    Simplify setup_gsutil

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,12 +3,15 @@
 set -ue
 
 function setup_gsutil() {
-  if [ -n "$GS_SERVICE_ACCOUNT_KEY" ]; then
-    echo "Setting up gsutil: writing contents of GS_SERVICE_ACCOUNT_KEY into /tmp/gcloud-key.json..."
-    echo $GS_SERVICE_ACCOUNT_KEY > /tmp/gcloud-key.json
-    gcloud auth activate-service-account --key-file /tmp/gcloud-key.json
-    echo "gsutil configured"
+  if [ -z "$GS_SERVICE_ACCOUNT_KEY" ]; then
+    echo "GS_SERVICE_ACCOUNT_KEY not configured. Skipping setup."
+    return 0
   fi
+
+  echo "Setting up gsutil: writing contents of GS_SERVICE_ACCOUNT_KEY into /tmp/gcloud-key.json..."
+  echo $GS_SERVICE_ACCOUNT_KEY > /tmp/gcloud-key.json
+  gcloud auth activate-service-account --key-file /tmp/gcloud-key.json
+  echo "gsutil configured"
 }
 
 function stellar_core_init_db() {
